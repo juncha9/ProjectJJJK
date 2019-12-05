d6b0d1bc52a5c59b99937a3866b758fe0c8958e6
Pulse-less <jhk0393@gmail.com>
2019-12-06 00:37:42 +0900

    191206 Ajax
    Ajax test 완료

--- routes/controller.js
+++ routes/controller.js
@@ -7,8 +7,19 @@ module.exports = function(app)
         res.render("index.ejs");
     });
 
-    
     app.use('/movie_test', movie_test);
     //컨트롤러 분리
+
+    /**
+     * jQuery ajax 방식 테스트
+     */
+
+     app.get("/test/jQueryAjaxGet",(req,res) =>{
+        res.render("testGet.ejs");
+     });
+
+     app.post("/test/jQueryAjaxPost",(req,res) =>{
+        res.render("testPost.ejs");
+     });
 }
 
