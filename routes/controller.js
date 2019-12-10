3e65df919d8bef881fe6d2431aa8c38daaf1156b
Pulse-less <jhk0393@gmail.com>
2019-12-10 23:21:53 +0900

    현규_191210_02
    기타수정

--- routes/controller.js
+++ routes/controller.js
@@ -18,8 +18,14 @@ module.exports = function(app)
    app.use('/movie_test', movie_test);
    //컨트롤러 분리
 
-   app.get("/movie/getPopularMovieList", (req,res) => {
-      db.query("select * from movie_info where release_date = 2019 order by user_rating desc limit 0,5;", function(err, result){
+   /**
+    * 20191210 현규
+    * 케러셀용
+    * streaming_cnt 순 5개 표시(연도제한없음)
+    * 
+    */
+   app.get("/movie/getMovieCarousel", (req,res) => {
+      db.query("select * from movie_info where delete_yn = 'N' order by streaming_cnt desc limit 0,5;", function(err, result){
          if(err){
             console.log(err);
             res.send(err);
@@ -30,7 +36,36 @@ module.exports = function(app)
       });
    });
 
+   /**
+    * 20191208 현규
+    * 실시간 인기 top5
+    * 현재 기준으로 인기top5를 보여줘야함
+    * 네이버에서 넘어오는 pubDate가 년도만 포함되어서
+    * 주차까지 분리하고싶었지만 데이터 한계상 2차가공이 필요함 -> drop결정
+    * new : 올해기준으로 streaming_cnt top5 보여주기
+    */
+   app.get("/movie/getPopularMovieList", (req,res) => {
+      //db.query("select * from movie_info where release_date = 2019 order by user_rating desc limit 0,5;", function(err, result){
+      db.query("select * from movie_info where release_date = 2019 and delete_yn = 'N' order by streaming_cnt desc limit 0,5;", function(err, result){
+         if(err){
+            console.log(err);
+            res.send(err);
+         }else{
+            console.log(result);
+            res.send(result);
+         }
+      });
+   });
+
+   /**
+    * 20191208 현규
+    * 최신영화
+    * 올해기준, 유저평점 높은 작품 5개를 보여줘야하지만
+    * 현재 movie_info 테이블의 데이터의 가공이 덜 끝났으므로
+    * 직접 선택한 5개만 뽑아서 보여주도록 작성
+    */
    app.get("/movie/getLatestMovieList",(req,res) => {
+      //db.query("select * from movie_info where release_date = 2019 order by user_rating desc limit 0,5;", function(err, result){
       db.query("select * from movie_info where movie_seq in(13,57,69,67,70);", function(err, result){
          if(err){
             console.log(err);
@@ -42,13 +77,12 @@ module.exports = function(app)
       });
    });
 
-   app.get("/insertMovie",(req,res)=>{
-      res.render("insertMovie.ejs");
-   });
-
-   //20191209 현규
-   //히든페이지에서 영화 검색시 디비로 자동 삽입...
-   //시간관계상 중복데이터 체크는 안합니다.
+   
+   /**
+    * 20191209 현규
+    * 히든페이지에서 영화 검색시 디비로 자동 삽입...
+    * 시간관계상 중복데이터 체크는 안합니다.
+    */
    app.get("/hidden/searchMovieForInsert",(req,res) =>{
       console.log(req.query.query);
       var api_url = 'https://openapi.naver.com/v1/search/movie?query=' + encodeURI(req.query.query); // json 결과
@@ -58,7 +92,7 @@ module.exports = function(app)
          url: api_url,
          headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
       };
-       request.get(options, function (error, response, body) {
+      request.get(options, function (error, response, body) {
          if (!error && response.statusCode == 200) {
             res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
             console.log(res);
@@ -70,7 +104,7 @@ module.exports = function(app)
          }
       });
    });
-
+   
    /**
     * 20191209 현규
     * 히든페이지 통해서 이쪽으로 접근하면
@@ -80,17 +114,27 @@ module.exports = function(app)
       let fn = async function(){
          try{
             await db.query("insert into movie_info(movie_name, movie_director, release_date, image_url, user_rating, insert_date) values (?,?,?,?,?,sysdate());",
-               [((req.query.title).replace("<b>","")).replace("</b>",""), req.query.director, req.query.pubDate, req.query.image, req.query.userRating]);
+            [((req.query.title).replace("<b>","")).replace("</b>",""), req.query.director, req.query.pubDate, req.query.image, req.query.userRating]);
          }catch(err){
             console.log(err);
             //res.redirect("/insertMovie");
          }
       }();
-
+      
       console.log(res);
       res.end();
    });
 
+   // app.get("/insertMovie",(req,res)=>{
+   //    res.render("insertMovie.ejs");
+   // });
+   
+   /**
+    * 20191210 현규
+    * 영화 상세보기
+    * bind된 movieSeq를 인자로 select후
+    * 랜더링을 통해 그림
+    */
    app.post("/getMovieView", (req,res) => {
       //console.log(req.body.movieSeq);
       //디비에서 해당 seq 영화정보 가져오기
